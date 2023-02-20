d3a1c37f9e031cd69b5f26345afde017d457912c
Henry <15941356654@163.com>
2023-02-20 23:46:27 +0800

    1、创建代码生成器 2、新增查询功能

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,42 @@
+package com.atguigu.eduservice.controller;
+
+
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduTeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author testjava
+ * @since 2023-02-20
+ */
+@RestController
+@RequestMapping("/eduservice/teacher")
+public class EduTeacherController {
+
+    //访问地址： http://localhost:8001/eduservice/teacher/findAll
+    //把service注入
+    @Autowired
+    private EduTeacherService teacherService;
+
+    //1 查询讲师表所有数据
+    //rest风格
+    @GetMapping("findAll")
+    public List<EduTeacher> findAllTeacher(){
+        List<EduTeacher> list = teacherService.list(null);
+        return list;
+    }
+
+
+
+}
+

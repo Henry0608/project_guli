a819f133bb5f760777c294cc7900e66ba2e189e1
Henry <15941356654@163.com>
2023-02-21 19:43:47 +0800

    整合swagger，测试逻辑删除

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -3,12 +3,13 @@ package com.atguigu.eduservice.controller;
 
 import com.atguigu.eduservice.entity.EduTeacher;
 import com.atguigu.eduservice.service.EduTeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
-
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 /**
@@ -19,6 +20,7 @@ import java.util.List;
  * @author testjava
  * @since 2023-02-20
  */
+@Api(description="讲师管理")
 @RestController
 @RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
@@ -30,10 +32,18 @@ public class EduTeacherController {
 
     //1 查询讲师表所有数据
     //rest风格
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping("findAll")
     public List<EduTeacher> findAllTeacher(){
         List<EduTeacher> list = teacherService.list(null);
         return list;
+    }
+    @ApiOperation(value = "逻辑删除讲师")
+    @DeleteMapping("{id}")
+    public boolean removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true)
+                                     @PathVariable String id){
+        boolean flag = teacherService.removeById(id);
+        return flag;
     }
 
 

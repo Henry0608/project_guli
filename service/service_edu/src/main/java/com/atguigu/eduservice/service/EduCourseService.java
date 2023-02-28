860866f34b612ae40b872a12ea8916b0e8f70502
Henry <15941356654@163.com>
2023-02-28 23:15:39 +0800

    增加发布页面接口

--- service/service_edu/src/main/java/com/atguigu/eduservice/service/EduCourseService.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/EduCourseService.java
@@ -2,6 +2,7 @@ package com.atguigu.eduservice.service;
 
 import com.atguigu.eduservice.entity.EduCourse;
 import com.atguigu.eduservice.entity.vo.CourseInfoVo;
+import com.atguigu.eduservice.entity.vo.CoursePublishVo;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
@@ -22,4 +23,6 @@ public interface EduCourseService extends IService<EduCourse> {
 
     //修改课程信息
     void updateCourseInfo(CourseInfoVo courseInfoVo);
+
+    CoursePublishVo publishCourseInfo(String id);
 }
--- service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduCourseServiceImpl.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduCourseServiceImpl.java
@@ -3,6 +3,7 @@ package com.atguigu.eduservice.service.impl;
 import com.atguigu.eduservice.entity.EduCourse;
 import com.atguigu.eduservice.entity.EduCourseDescription;
 import com.atguigu.eduservice.entity.vo.CourseInfoVo;
+import com.atguigu.eduservice.entity.vo.CoursePublishVo;
 import com.atguigu.eduservice.mapper.EduCourseMapper;
 import com.atguigu.eduservice.service.EduCourseDescriptionService;
 import com.atguigu.eduservice.service.EduCourseService;
@@ -91,5 +92,12 @@ public class EduCourseServiceImpl extends ServiceImpl<EduCourseMapper, EduCourse
         description.setDescription(courseInfoVo.getDescription());
         courseDescriptionService.updateById(description);
 
+    }
+
+    @Override
+    public CoursePublishVo publishCourseInfo(String id) {
+        //调用mapper
+        CoursePublishVo publishCourseInfo = baseMapper.getPublishCourseInfo(id);
+        return publishCourseInfo;
     }
 }

8e7b745b292a07c7c3de29163b038ab2afbc9862
Henry <15941356654@163.com>
2023-02-28 22:09:39 +0800

    小节的增删改查接口

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduVideoController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduVideoController.java
@@ -1,9 +1,12 @@
 package com.atguigu.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduChapter;
+import com.atguigu.eduservice.entity.EduVideo;
+import com.atguigu.eduservice.service.EduVideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -14,8 +17,41 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2023-02-27
  */
 @RestController
-@RequestMapping("/eduservice/edu-video")
+@RequestMapping("/eduservice/video")
+@CrossOrigin
 public class EduVideoController {
+
+    @Autowired
+    private EduVideoService videoService;
+
+    //添加小节
+    @PostMapping("addVideo")
+    public R addVideo(@RequestBody EduVideo eduVideo) {
+        videoService.save(eduVideo);
+        return R.ok();
+    }
+
+    //删除小节
+    // TODO 后面这个方法需要完善：删除小节时候，同时把里面视频删除
+    @DeleteMapping("{id}")
+    public R deleteVideo(@PathVariable String id) {
+        videoService.removeById(id);
+        return R.ok();
+    }
+
+    //根据小杰id查询
+    @GetMapping("getVideoInfo/{videoId}")
+    public R getVideoInfo(@PathVariable String videoId){
+        EduVideo eduVideo = videoService.getById(videoId);
+        return R.ok().data("video",eduVideo);
+    }
+    //修改小节
+    @PostMapping("updateVideo")
+    public R updateVideo(@RequestBody EduVideo eduVideo){
+        videoService.updateById(eduVideo);
+        return R.ok();
+
+    }
 
 }
 

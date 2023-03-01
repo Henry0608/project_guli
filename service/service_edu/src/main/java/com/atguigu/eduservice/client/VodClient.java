82d7c72e3bf9f9c00cde10b68f07c41494e16ea8
Henry <15941356654@163.com>
2023-03-01 23:05:35 +0800

    用feign调用实现删除课程同时删除小节中的多个视频

--- service/service_edu/src/main/java/com/atguigu/eduservice/client/VodClient.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/client/VodClient.java
@@ -4,6 +4,9 @@ import com.atguigu.commonutils.R;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @FeignClient(name = "service-vod") //调用的服务名称
 public interface VodClient {
@@ -13,4 +16,11 @@ public interface VodClient {
     //@PathVariable注解一定要指定参数名称，否则出错
     @DeleteMapping("/eduvod/video/removeAlyVideo/{id}")
     public R removeAlyVideo(@PathVariable("id") String id);
+
+    //定义调用删除多个视频的方法
+    //删除多个阿里云视频的方法
+    //参数多个视频id  List videoIdList
+    @DeleteMapping("/eduvod/video/delete-batch")
+    public R deleteBatch(@RequestParam("videoIdList") List<String> videoIdList);
+
 }
--- service/service_vod/src/main/java/com/atguigu/vod/service/impl/VodServiceImpl.java
+++ service/service_vod/src/main/java/com/atguigu/vod/service/impl/VodServiceImpl.java
@@ -1,14 +1,20 @@
 package com.atguigu.vod.service.impl;
 
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.vod.model.v20170321.DeleteVideoRequest;
+import com.atguigu.servicebase.exceptionhandler.GuliException;
 import com.atguigu.vod.Utils.ConstantVodUtils;
+import com.atguigu.vod.Utils.InitVodCilent;
 import com.atguigu.vod.service.VodService;
 import com.aliyun.vod.upload.impl.UploadVideoImpl;
 import com.aliyun.vod.upload.req.UploadStreamRequest;
 import com.aliyun.vod.upload.resp.UploadStreamResponse;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
+import java.util.List;
 
 @Service
 public class VodServiceImpl implements VodService {
@@ -39,6 +45,29 @@ public class VodServiceImpl implements VodService {
         } catch (Exception e) {
             e.printStackTrace();
             return null;
+        }
+
+    }
+
+    // 删除多个阿里云视频
+    @Override
+    public void removeMoreAlyVideo(List<String> videoIdList) {
+        try {
+            //初始化对象
+            DefaultAcsClient client = InitVodCilent.initVodClient(ConstantVodUtils.ACCESS_KEY_ID, ConstantVodUtils.ACCESS_KEY_SECRET);
+            //创建删除视频request对象
+            DeleteVideoRequest request = new DeleteVideoRequest();
+
+            //videoIdList值转换成 1,2,3
+            String videoIds = StringUtils.join(videoIdList.toArray(), ",");
+
+            //向request设置视频id
+            request.setVideoIds(videoIds);
+            //调用初始化对象的方法实现删除
+            client.getAcsResponse(request);
+        }catch(Exception e) {
+            e.printStackTrace();
+            throw new GuliException(20001,"删除视频失败");
         }
 
     }

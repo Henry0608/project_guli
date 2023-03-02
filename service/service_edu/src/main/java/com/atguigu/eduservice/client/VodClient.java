87ecc589b4cd12a6e8a3bd1ef0e7596f1e9a028e
Henry <15941356654@163.com>
2023-03-02 17:33:36 +0800

    增加Feign调用注解

--- service/service_edu/src/main/java/com/atguigu/eduservice/client/VodClient.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/client/VodClient.java
@@ -2,6 +2,7 @@ package com.atguigu.eduservice.client;
 
 import com.atguigu.commonutils.R;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @FeignClient(name = "service-vod",fallback = VodFileDegradeFeignClient.class) //调用的服务名称,如果失败了调用熔断器的类
+@Component
 public interface VodClient {
 
     //定义调用的方法路径
--- service/service_edu/src/main/java/com/atguigu/eduservice/client/VodFileDegradeFeignClient.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/client/VodFileDegradeFeignClient.java
@@ -1,9 +1,11 @@
 package com.atguigu.eduservice.client;
 
 import com.atguigu.commonutils.R;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class VodFileDegradeFeignClient implements VodClient{
     //出错之后会执行
     @Override
